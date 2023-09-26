43f9af41a0b9d3fb4e417597d686dcf0c12d9a95
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-09-26 16:07:55 +0530

    new project added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todos"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
